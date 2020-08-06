d221855f7665e47678e11f2da55d3fda0834b9c8
Njaal Wiik <njaal.wiik@gmail.com>
2020-08-06 23:24:13 +0200

    CommentForm and adding comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -151,3 +151,22 @@ export const addComment = (postId, formData) => async dispatch => {
     });
   }
 };
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
